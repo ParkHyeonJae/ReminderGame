bcc5e687c1d8f88d054c5942f9e1b0e95e366cf3
ParkHyeonJae <md101106@gmail.com>
2020-06-29 01:18:52 +0900

    ADD: Second versions

---
+++ Assets/AudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlaySound(audioClip, audioSource);
+    }
+
+    public static void PlaySound(AudioClip audioClip, AudioSource audioSource)
+    {
+        audioSource.clip = audioClip;
+        
+        audioSource.volume = 1.0f;
+        audioSource.loop = true;
+        audioSource.time = 0;
+        audioSource.Play();
+
+    }
+}
